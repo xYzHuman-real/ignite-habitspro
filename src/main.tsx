59f940b856d39adb188fe7c8970a3ac810984246
xYzHuman-real <xyzhuman.real@gmail.com>
2026-09-05 13:44:09 +0530

    enable reliable ambient sounds in WebView

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { installAmbientSoundFallback } from "./lib/ambient-sounds";
 
+installAmbientSoundFallback();
 createRoot(document.getElementById("root")!).render(<App />);
