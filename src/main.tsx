5ca99a8ccaa047e8b2b83a5dd93657235ca26fdb
xYzHuman-real <xyzhuman.real@gmail.com>
2026-09-05 13:45:01 +0530

    connect web timer state to native focus guard

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { installAmbientSoundFallback } from "./lib/ambient-sounds";
+import { installNativeFocusBridge } from "./lib/native-focus-bridge";
 
 installAmbientSoundFallback();
+installNativeFocusBridge();
 createRoot(document.getElementById("root")!).render(<App />);
